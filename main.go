4802ab19b9c2ac36f63975d1d825f4e6484cde52
Anais1210 <azhang@myges.fr>
2022-11-10 12:54:58 +0100

    correction of exercice1

--- main.go
+++ main.go
@@ -4,31 +4,38 @@ import (
 	"fmt"
 )
 
-
 type City struct {
-    city string
-    population float64
-    zipcode string
-    surface float64
-    
+    cityname string
+    population uint
+    zipcode uint
+    surface uint
 }
-func city(){
 
-    var city City
-    fmt.Println("Enter city name")
-    fmt.Scanln(&city.city)
-    fmt.Println("Enter population")
-    fmt.Scanln(&city.population)
-    fmt.Println("Enter zipcode")
-    fmt.Scanln(&city.zipcode)
-    fmt.Println("Enter surface")
-    fmt.Scanln(&city.surface)
-    fmt.Println(city)
-       
+func city(){
+    var mapping = map[string]City{}
+    var numberofcities int
+    fmt.Println("How many cities do you want to enter?")
+    fmt.Scanln(&numberofcities)
+    fmt.Println("Number of cities: ", numberofcities)
+
+    for i := 0; i < numberofcities; i++ {
+        var cityname string
+        var population uint
+        var zipcode uint
+        var surface uint
+        fmt.Println("Enter the name of the city: ")
+        fmt.Scanln(&cityname)
+        fmt.Println("Enter the population of the city: ")
+        fmt.Scanln(&population)
+        fmt.Println("Enter the zipcode of the city: ")
+        fmt.Scanln(&zipcode)
+        fmt.Println("Enter the surface of the city: ")
+        fmt.Scanln(&surface)
+        mapping[cityname] = City{cityname, population, zipcode, surface}
+    }
+    fmt.Println("Mapping: ", mapping)
 }
 
 func main(){
-    
-    fmt.Println("Hello World")
     city()
 }
