0dd135e739b7445e4211c0b4f14098217d418ef1
Anais1210 <azhang@myges.fr>
2022-11-10 14:56:12 +0100

    exercice2 with transaction per time calcul

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func transaction(){
     fmt.Println("Transaction number: ")
     fmt.Scanln(&transactionNumber)
 
-    
+
     var choice int
     fmt.Println("1. Seconde")
     fmt.Println("2. Minute")
@@ -19,21 +19,23 @@ func transaction(){
 
     switch choice {
     case 1:
-    var seconde int
-    fmt.Scanln("Enter: ")
-    fmt.Scanf("%d", &seconde)
-    fmt.Printf("Transaction by seconde %d for %d: ", transactionNumber, seconde) 
+        var second int
+        fmt.Scanln("How many seconds?: ")
+        fmt.Scanf("%d", &second)
+        fmt.Printf("%d transactions per seconds", transactionNumber/second) 
     case 2:
-    var minute int
-    fmt.Scanln("Enter: ")
-    fmt.Scanf("%d", &minute)
-    fmt.Printf("Transaction by minute %d for %d: ", transactionNumber, minute) 
-    fmt.Println("transaction by minute.")
+        var minute int
+        fmt.Scanln("How many minutes?: ")
+        fmt.Scanf("%d", &minute)
+        fmt.Printf("%d transactions by minute ", transactionNumber/minute) 
+        fmt.Println("transaction by minute.")
     case 3:
-    var hour int
-    fmt.Scanln("Enter: ")
-    fmt.Scanf("%d", &hour)
-    fmt.Printf("Transaction by hour %d for %d: ", transactionNumber, hour) 
+        var hour int
+        fmt.Scanln("How many hours?: ")
+        fmt.Scanf("%d", &hour)
+        fmt.Printf("%d transactions by hour ", transactionNumber/hour) 
+    default:
+        fmt.Println("Invalid choice")
     }
 
 }
