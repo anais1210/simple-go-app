bc71c02c6b3dbe426c7b45e9a79863f3113eb3f0
Anais1210 <azhang@myges.fr>
2022-11-10 15:21:57 +0100

    exercice 1-3

--- main.go
+++ main.go
@@ -2,7 +2,39 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
+type City struct {
+    cityname string
+    population uint
+    zipcode uint
+    surface uint
+}
+
+func city(){
+    var mapping = map[string]City{}
+    var numberofcities int
+    fmt.Println("How many cities do you want to enter?")
+    fmt.Scanln(&numberofcities)
+    fmt.Println("Number of cities: ", numberofcities)
+
+    for i := 0; i < numberofcities; i++ {
+        var cityname string
+        var population uint
+        var zipcode uint
+        var surface uint
+        fmt.Println("Enter the name of the city: ")
+        fmt.Scanln(&cityname)
+        fmt.Println("Enter the population of the city: ")
+        fmt.Scanln(&population)
+        fmt.Println("Enter the zipcode of the city: ")
+        fmt.Scanln(&zipcode)
+        fmt.Println("Enter the surface of the city: ")
+        fmt.Scanln(&surface)
+        mapping[cityname] = City{cityname, population, zipcode, surface}
+    }
+    fmt.Println("Mapping: ", mapping)
+}
 
 func transaction(){
     var transactionNumber int
@@ -39,7 +71,25 @@ func transaction(){
     }
 
 }
+func sortName(){
+    var name string
+    var nameList []string
+    for {
+        fmt.Println("Enter a name:")
+        fmt.Scanf("%s", &name)
+        nameList = append(nameList, name)
+        if(name == "X"){
+            break
+        }
+    }
+    sort.Strings(nameList)
+    fmt.Printf("%v",nameList)
+}
+
 
 func main(){
+    city()
     transaction()
+    sortName()
+
 }
