84d499d20005518d994de38a7ed263d902f6fbd9
Anais1210 <azhang@myges.fr>
2022-11-10 15:11:17 +0100

    exercice4 done

--- main.go
+++ main.go
@@ -2,44 +2,27 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
+func printSlice(s []string) {
+    fmt.Printf("len=%d cap=%d %v", len(s),cap(s), s)
+}
 
-func transaction(){
-    var transactionNumber int
-    fmt.Println("Transaction number: ")
-    fmt.Scanln(&transactionNumber)
-
-
-    var choice int
-    fmt.Println("1. Seconde")
-    fmt.Println("2. Minute")
-    fmt.Println("3. Hour")
-    fmt.Scanf("%d", &choice)
-
-
-    switch choice {
-    case 1:
-        var second int
-        fmt.Scanln("How many seconds?: ")
-        fmt.Scanf("%d", &second)
-        fmt.Printf("%d transactions per seconds", transactionNumber/second) 
-    case 2:
-        var minute int
-        fmt.Scanln("How many minutes?: ")
-        fmt.Scanf("%d", &minute)
-        fmt.Printf("%d transactions by minute ", transactionNumber/minute) 
-        fmt.Println("transaction by minute.")
-    case 3:
-        var hour int
-        fmt.Scanln("How many hours?: ")
-        fmt.Scanf("%d", &hour)
-        fmt.Printf("%d transactions by hour ", transactionNumber/hour) 
-    default:
-        fmt.Println("Invalid choice")
+func sortName(){
+    var name string
+    var nameList []string
+    for {
+        fmt.Println("Enter a name:")
+        fmt.Scanf("%s", &name)
+        nameList = append(nameList, name)
+        if(name == "X"){
+            break
+        }
     }
-
+    sort.Strings(nameList)
+    fmt.Printf("%v",nameList)
 }
 
 func main(){
-    transaction()
+    sortName()
 }
